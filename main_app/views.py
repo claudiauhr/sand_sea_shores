dfb3e6ca98f5abacd9b8154554d01b897c4e95c4
claudiauhr <claudia.c.uhr@gmail.com>
2023-02-07 19:57:05 -0500

    wip: first models.py added

--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Shore
+
 
 def home(request):
-    return HttpResponse('<h1>In the sea <º)))><</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def shores_index(request):
+    shores = Shore.objects.all()
     return render(request, 'shores/index.html', {'shores': shores})
 
-class Shore:
-    def __init__(self, name, place, description):
-        self.name = name
-        self.place = place
-        self.description = description
+# class Shore:
+#     def __init__(self, name, place, description):
+#         self.name = name
+#         self.place = place
+#         self.description = description
 
-shores = [
-    Shore('Porto de Galinhas', 'Ipojuca', 'Lorem ipsum dolor sit amet.'),
-    Shore('Porto de Galinhas2', 'Ipojuca', 'Lorem ipsum dolor sit amet.'),
-    Shore('Porto de Galinhas3', 'Ipojuca', 'Lorem ipsum dolor sit amet.'),
-    Shore('Porto de Galinhas4', 'Ipojuca', 'Lorem ipsum dolor sit amet.')
-]
+# shores = [
+#     Shore('Porto de Galinhas', 'Ipojuca', 'Lorem ipsum dolor sit amet.'),
+#     Shore('Porto de Galinhas2', 'Ipojuca', 'Lorem ipsum dolor sit amet.'),
+#     Shore('Porto de Galinhas3', 'Ipojuca', 'Lorem ipsum dolor sit amet.'),
+#     Shore('Porto de Galinhas4', 'Ipojuca', 'Lorem ipsum dolor sit amet.')
+# ]
+
