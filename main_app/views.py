6de340e4b37c0f2a1bcf4b84f4eff3ba700eed28
claudiauhr <claudia.c.uhr@gmail.com>
2023-02-07 19:26:36 -0500

    wip: added all Shores page

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,19 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def shores_index(request):
+    return render(request, 'shores/index.html', {'shores': shores})
+
+class Shore:
+    def __init__(self, name, place, description):
+        self.name = name
+        self.place = place
+        self.description = description
+
+shores = [
+    Shore('Porto de Galinhas', 'Ipojuca', 'Lorem ipsum dolor sit amet.'),
+    Shore('Porto de Galinhas2', 'Ipojuca', 'Lorem ipsum dolor sit amet.'),
+    Shore('Porto de Galinhas3', 'Ipojuca', 'Lorem ipsum dolor sit amet.'),
+    Shore('Porto de Galinhas4', 'Ipojuca', 'Lorem ipsum dolor sit amet.')
+]
